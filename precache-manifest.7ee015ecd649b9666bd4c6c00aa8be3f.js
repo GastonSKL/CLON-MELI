3262c17411bfa927ac9ca9edbe477639b86fcc07
GastonSKL <gastonlskamlec@gmail.com>
2022-11-04 11:59:28 -0300

    Updates

---
+++ precache-manifest.7ee015ecd649b9666bd4c6c00aa8be3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "18997c71c812b90e61ad",
+    "url": "/CLON-MELI/static/css/main.e13e3491.chunk.css"
+  },
+  {
+    "revision": "18997c71c812b90e61ad",
+    "url": "/CLON-MELI/static/js/main.18997c71.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/CLON-MELI/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "b26a1c6615e58f0f6697",
+    "url": "/CLON-MELI/static/js/2.b26a1c66.chunk.js"
+  },
+  {
+    "revision": "fbb6b711c615cb10ede0",
+    "url": "/CLON-MELI/static/js/runtime~main.fbb6b711.js"
+  },
+  {
+    "revision": "0e36e430d3b39eef97137e0a1f384232",
+    "url": "/CLON-MELI/static/media/profile.0e36e430.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CLON-MELI/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2c1e733e1288eb5dece08765d85be56c",
+    "url": "/CLON-MELI/index.html"
+  }
+];
